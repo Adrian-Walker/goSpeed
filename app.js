fe34176eb85f4580b619407cd8bc34823b3c3478
Adrian Walker <awalker3365@gmail.com>
2021-04-14 14:16:22 -0400

    2nd

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-document.getElementById("myButton", function(){
-    
+let count =localStorage.clickcount = Number(localStorage.clickcount) + 1
+let numberOfClicks = localStorage.getItem("clicks", 1)
+
+
+document.addEventListener("click", function () {
+    document.getElementById("myButton")
+    console.log(count++)
+
+
+    const create = document.createElement('h2')
+    create.textContent = count
+    document.getElementsByTagName("div")[0].append(create)
+
 })
